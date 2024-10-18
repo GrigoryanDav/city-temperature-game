c7a42270f103cfe34d61d1cc57181a4aacb937b9
GrigoryanDav <grigoryand219@gmail.com>
2024-10-18 16:48:32 +0400

    Create GuessBlock component

---
+++ src/components/GuessBlock/index.js
@@ -0,0 +1,29 @@
+import { Typography, Input, Button } from 'antd'
+
+
+const { Title } = Typography
+
+const GuessBlock = ({ currentCity, userTemperature, setUserTemperature, handleCheck }) => {
+    return (
+        <div className='guess_block'>
+            <Title level={3}>City Game</Title>
+            <Title level={4}>{currentCity}</Title>
+            <Input
+                type='number'
+                value={userTemperature}
+                onChange={(e) => setUserTemperature(e.target.value)}
+                placeholder="Enter temperature"
+            />
+            <hr />
+            <Button
+                type='primary'
+                onClick={handleCheck}
+                disabled={!userTemperature}
+            >
+                Check
+            </Button>
+        </div>
+    )
+}
+
+export default GuessBlock
