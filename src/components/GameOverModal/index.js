3f4da3357fba320e01ba5ac9c8076ca698deed78
GrigoryanDav <grigoryand219@gmail.com>
2024-10-18 17:01:36 +0400

    Implement GameOverModal functionality

---
+++ src/components/GameOverModal/index.js
@@ -0,0 +1,19 @@
+import { Modal, Typography } from 'antd'
+
+
+const { Text } = Typography
+
+const GameOverModal = ({ isModalVisible, hasWon, handleModalClose }) => {
+    return (
+        <Modal
+        title="Game Over"
+        open={isModalVisible}
+        onOk={handleModalClose}
+        onCancel={handleModalClose}
+        >
+            <Text>{hasWon ? 'Congratulations! You Won!' : 'Sorry, you lost. Try again!'}</Text>
+        </Modal>
+    )
+}
+
+export default GameOverModal
